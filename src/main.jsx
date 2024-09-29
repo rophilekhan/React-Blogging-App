3b12cf64425ddd5b61195172dced69af1f47371b
Rophile Ahmed Khan <rophile.khan@gmail.com>
2024-09-30 02:13:22 +0500

    Working on main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Home from './pages/Home.jsx'
+import Dashbord from './pages/Dashbord.jsx'
+import ProtectedRout from './'
+import Profile from './components/pages/Profile.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <Layout />,
+    children : [
+      {
+        path : '/',
+        element: <Home />
+      },
+      {
+        path: 'login',
+        element: <Login/>
+      },
+      {
+        path: 'register',
+        element: <Register/>
+      },
+      {
+        path : 'dashbord',
+        element :<ProtectedRout component={<Dashbord/>}/>
+      },
+      {
+        path : 'profile',
+        element :<ProtectedRout component={<Profile/>}/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}>
+    <Layout />
+  </RouterProvider>
 )
